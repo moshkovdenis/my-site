77f7fa3f4327c96ff304022547dc434ac535fc83
DenisJavaMoshcov <moshcovdenis@mail.ru>
2022-05-12 18:43:28 +0300

    add comment test

--- back/user-service/src/main/java/ru/denis/photosite/services/UserService.java
+++ back/user-service/src/main/java/ru/denis/photosite/services/UserService.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Transactional
 public class UserService {
+    //test
     private final UserRepository userRepository;
 
     public void createUser(Client client) {
